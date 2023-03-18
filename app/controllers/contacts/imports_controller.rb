4b2bcdbefac5e476809cd7ef741fba837fc08d02
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-03-18 23:51:53 +0500

    Handle exception

--- app/controllers/contacts/imports_controller.rb
+++ app/controllers/contacts/imports_controller.rb
@@ -6,57 +6,51 @@ class Contacts::ImportsController < ApplicationController
   end
 
   def create
-    
-    respond_to do |format|
-      # unless params[:import_file]
-      #   # redirect_to contact_url
-      #   format.html { redirect_to new_contact_import_path, status: :unprocessable_entity, alert: "Contact was successfully updated." }
-      #   format.turbo_stream do
-      #     render turbo_stream: turbo_stream.replace('notification', partial: 'contacts/imports/notice',
-      #                                                               locals: { alert: 'Please attach a vcard file.' })
-      #   end
-      #   return
-      # end
-      # puts params[:import_file]
-      puts params[:import_file]
-      @vcard_import = Vcard::Vcard.decode(params[:import_file].read) if params[:import_file]
-      @errors = []
-      @success = 0
+    @errors = []
+    @success = 0
 
-      @vcard_import.each do |vcard|
-        @contact = Contact.new
-        @contact.name = vcard.name.fullname
-        @contact.email_address = vcard.email
-        if @contact.save
-          # DO NOTHING // CONTINUE
-          @success += 1
-        else
-          contact_error = { "#{@contact.name}": @contact.errors }
-          @errors.push(contact_error)
-          # errors.push(@contact.errors) # TO DO SOMETHING
+    begin
+      @vcard_import = Vcard::Vcard.decode(params[:import_file].read) if params[:import_file]
+    rescue Vcard::InvalidEncodingError => e
+      @errors.push({InvalidCard: [full_message: "This VCard is invalid"]})
+    end
+
+    respond_to do |format|
+      
+      if @errors.count <= 0
+        @vcard_import.each do |vcard|
+          @contact = Contact.new
+          @contact.name = vcard.name.fullname
+          @contact.email_address = vcard.email
+          if @contact.save
+            # DO NOTHING // CONTINUE
+            @success += 1
+          else
+            contact_error = { "#{@contact.name}": @contact.errors }
+            @errors.push(contact_error)
+            # errors.push(@contact.errors) # TO DO SOMETHING
+          end
         end
       end
 
       if @errors.count > 0
-        flash[:notice] = "Contact removed"
         format.turbo_stream do
           render turbo_stream: [
             turbo_stream.replace('notification', partial: 'contacts/imports/notice',
                                                  locals: { alert: 'Contact import completed with errors.' }),
             turbo_stream.replace('import_form', partial: 'contacts/imports/form',
-                                                locals: { is_show_action: true, contact: Contact.new }),
+                                                locals: { contact: Contact.new }),
             turbo_stream.replace('import_result', partial: 'contacts/imports/errors',
                                                 locals: { success: @success, errors: @errors })
           ]
         end
       else
-        flash[:notice] = "Contact removed"
         format.turbo_stream do
           render turbo_stream: [
             turbo_stream.replace('notification', partial: 'contacts/imports/notice',
                                                  locals: { notice: 'Contact import completed with no errors.' }),
             turbo_stream.replace('import_form', partial: 'contacts/imports/form',
-                                                locals: { is_show_action: true, contact: Contact.new }),
+                                                locals: { contact: Contact.new }),
             turbo_stream.replace('import_result', partial: 'contacts/imports/errors',
                                                 locals: { success: @success, errors: @errors })
           ]
